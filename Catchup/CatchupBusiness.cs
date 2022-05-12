f451c1b24a0f9f067dcecddebe8267e191b4b1df
Alireza Mortezaei <testmail@test.com>
2022-05-13 01:44:06 +0430

    Captcha generator and verification

---
+++ Catchup/CatchupBusiness.cs
@@ -0,0 +1,64 @@
+﻿namespace Catchup
+{
+	public class CatchupBusiness
+	{
+		private readonly string[] _singleDigits = { "یک", "دو", "سه", "چهار", "پنج", "شش", "هفت", "هشت", "نه" };
+		private readonly int[] _singleDigitsMatch = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+		private readonly string[] _twoDigits = { "ده", "یازده", "دوازده", "سیزده", "چهارده", "پانزده", "شانزده", "هفده", "هجده", "نوزده", "بیست", "سی", "چهل", "پنجاه", "شصت", "هفتاد", "هشتاد", "نود" };
+		private readonly int[] _twoDigitsMatch = { 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 30, 40, 50, 60, 70, 80, 90 };
+		private readonly string[] _threeDigits = { "صد", "دویست", "سیصد", "چهارصد", "پانصد", "ششصد", "هفتصد", "هشتصد", "نهصد" };
+		private readonly int[] _threeDigitsMatch = { 100, 200, 300, 400, 500, 600, 700, 800, 900 };
+		private readonly Random _random = new();
+
+		public string GetACaptcha()
+		{
+			int single = _random.Next(_singleDigits.Length);
+			int two = _random.Next(_twoDigits.Length);
+			int three = _random.Next(_threeDigits.Length);
+			int singleForThousand = _random.Next(_singleDigits.Length);
+
+			var result = _singleDigits[singleForThousand] + " هزار و " + _threeDigits[three] + " و " + _twoDigits[two] + (two < 10 ? "" : $" و {_singleDigits[single]}");
+			return result;
+		}
+
+		public bool CheckCaptcha(string riddle, string solution) =>
+			Analyze(riddle).Equals(solution);
+
+		private string Analyze(string riddle)
+		{
+			try
+			{
+				riddle = riddle.Replace("هزار", "");
+				var riddleArray = riddle.Split(" ");
+				riddleArray = riddleArray.Where(x => !string.IsNullOrEmpty(x) && !x.Equals("و")).ToArray();
+
+				int thousandIndex = Array.IndexOf(_singleDigits, riddleArray[0]);
+				int hundredIndex = Array.IndexOf(_threeDigits, riddleArray[1]);
+				int decimalIndex = Array.IndexOf(_twoDigits, riddleArray[2]);
+				int singleIndex = -1;
+				if (riddleArray.Length > 3)
+					singleIndex = Array.IndexOf(_singleDigits, riddleArray[3]);
+
+				return FindNumberMatch(thousandIndex, hundredIndex, decimalIndex, singleIndex).ToString();
+			}
+			catch
+			{
+				return "-1111";
+			}
+
+		}
+
+		private int FindNumberMatch(int thousand, int hundred, int dec, int single)
+		{
+			int forth = int.Parse(_singleDigitsMatch[thousand].ToString() + "000");
+			int third = _threeDigitsMatch[hundred];
+			int second = _twoDigitsMatch[dec];
+			int first = 0;
+			if (single > -1)
+				first = _singleDigitsMatch[single];
+
+			return first + second + third + forth;
+		}
+
+	}
+}
---
+++ Execute/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Catchup.CatchupBusiness catchupBusiness = new Catchup.CatchupBusiness();
+
+var puzzle = catchupBusiness.GetACaptcha();
+
+Console.WriteLine(puzzle);
+var answer = "";
+bool result = false;
+
+do
+{
+	answer = Console.ReadLine();
+	result = catchupBusiness.CheckCaptcha(puzzle, answer);
+	Console.WriteLine(result);
+}
+while (!result);
