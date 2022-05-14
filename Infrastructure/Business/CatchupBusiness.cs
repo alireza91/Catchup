8d354b93348288d905d1befddfb788e5189ceee9
Alireza Mortezaei <testmail@test.com>
2022-05-14 15:14:58 +0430

    remove commented out codes

--- Infrastructure/Business/CatchupBusiness.cs
+++ Infrastructure/Business/CatchupBusiness.cs
@@ -5,11 +5,9 @@ namespace Infrastructure
 	public class CatchupBusiness : ICatchupBusiness
 	{
 		private readonly string[] _singleDigits = { "یک", "دو", "سه", "چهار", "پنج", "شش", "هفت", "هشت", "نه" };
-		//private readonly int[] _singleDigitsMatch = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 		private readonly string[] _twoDigits = { "ده", "یازده", "دوازده", "سیزده", "چهارده", "پانزده", "شانزده", "هفده", "هجده", "نوزده", "بیست", "سی", "چهل", "پنجاه", "شصت", "هفتاد", "هشتاد", "نود" };
 		private readonly int[] _twoDigitsMatch = { 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 30, 40, 50, 60, 70, 80, 90 };
 		private readonly string[] _threeDigits = { "صد", "دویست", "سیصد", "چهارصد", "پانصد", "ششصد", "هفتصد", "هشتصد", "نهصد" };
-		//private readonly int[] _threeDigitsMatch = { 100, 200, 300, 400, 500, 600, 700, 800, 900 };
 		private readonly Random _random = new();
 
 		public string GetACaptcha()
@@ -52,7 +50,6 @@ namespace Infrastructure
 		private int FindNumberMatch(int thousand, int hundred, int dec, int single)
 		{
 			int forth = int.Parse((thousand + 1).ToString() + "000");
-			//int third = _threeDigitsMatch[hundred];
 			int third = (hundred + 1) * 100;
 			int second = _twoDigitsMatch[dec];
 			int first = 0;
