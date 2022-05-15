73d43b07e0bca42fdb5e727094cab93cccb3792f
Alireza Mortezaei <testmail@test.com>
2022-05-16 00:15:10 +0430

    [feature] - Add ability to get the generated captcha as image, add regions to Catchup class

--- Catchup/Business/Catchup.cs
+++ Catchup/Business/Catchup.cs
@@ -7,41 +7,70 @@ using System.Text;
 namespace Catchup;
 public class Catchup : ICatchup
 {
+	#region [Field(s)]
+	
 	private readonly string[] _singleDigits = { "یک", "دو", "سه", "چهار", "پنج", "شش", "هفت", "هشت", "نه" };
 	private readonly string[] _twoDigits = { "ده", "یازده", "دوازده", "سیزده", "چهارده", "پانزده", "شانزده", "هفده", "هجده", "نوزده", "بیست", "سی", "چهل", "پنجاه", "شصت", "هفتاد", "هشتاد", "نود" };
 	private readonly string[] _threeDigits = { "صد", "دویست", "سیصد", "چهارصد", "پانصد", "ششصد", "هفتصد", "هشتصد", "نهصد" };
 
 	private readonly short[] _twoDigitsMatch = { 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 30, 40, 50, 60, 70, 80, 90 };
 	private readonly Random _random = new();
-	private readonly short _width = 400;
+	private readonly short _width = 1400;
 	private readonly short _height = 100;
-	private readonly StringBuilder sb = new();
+	private readonly StringBuilder _sb = new();
+
+	#endregion
+
+	#region [Public method(s)]
 
 	public string GetACaptcha()
+	{
+		return GenerateRandomCaptcha();
+	}
+
+	public Bitmap GetAnImageCaptchaInBitmapFormat()
+	{
+		string generatedCaptchaString = GenerateRandomCaptcha();
+		return MakeCaptchaImage(generatedCaptchaString);
+	}
+
+	public byte[] GetAnImageCaptchaInByteArray()
+	{
+		string generatedCaptchaString = GenerateRandomCaptcha();
+		var image = MakeCaptchaImage(generatedCaptchaString);
+		ImageConverter converter = new ImageConverter();
+		return (byte[])converter.ConvertTo(image, typeof(byte[]));
+	}
+
+	public bool CheckCaptcha(string riddle, string solution) =>
+		Analyze(riddle).Equals(solution);
+
+	#endregion
+
+	#region [Private method(s)]
+
+	private string GenerateRandomCaptcha()
 	{
 		int single = _random.Next(_singleDigits.Length);
 		int two = _random.Next(_twoDigits.Length);
 		int three = _random.Next(_threeDigits.Length);
 		int singleForThousand = _random.Next(_singleDigits.Length);
-		
-		var words = new string[] 
+
+		var words = new string[]
 		{
 			_singleDigits[singleForThousand],
 			" هزار و ",
-			_threeDigits[three], 
+			_threeDigits[three],
 			" و ",
 			_twoDigits[two],
 			two < 10 ? "" : $" و {_singleDigits[single]}"
 		};
 
 		for (short i = 0; i < words.Length; i++)
-			sb.Append(words[i]);
+			_sb.Append(words[i]);
 
-		return sb.ToString();
+		return _sb.ToString();
 	}
-
-	public bool CheckCaptcha(string riddle, string solution) =>
-		Analyze(riddle).Equals(solution);
 
 	private string Analyze(string riddle)
 	{
@@ -71,7 +100,7 @@ public class Catchup : ICatchup
 		return first + second + third + forth;
 	}
 
-	private Bitmap generateCaptchaImage(string riddleString)
+	private Bitmap MakeCaptchaImage(string riddleString)
 	{
 		//First declare a bitmap and declare graphic from this bitmap
 		Bitmap bitmap = new Bitmap(_width, _height, PixelFormat.Format32bppArgb);
@@ -85,9 +114,9 @@ public class Catchup : ICatchup
 		//here we make the text configurations
 		GraphicsPath graphicPath = new GraphicsPath();
 		//add this string to image with the rectangle delegate
-		graphicPath.AddString(riddleString, FontFamily.GenericMonospace, (int)FontStyle.Bold, 90, rect, null);
+		graphicPath.AddString(riddleString, FontFamily.GenericMonospace, (int)FontStyle.Bold, 80, rect, null);
 		//And the brush that you will write the text
-		hatchBrush = new HatchBrush(HatchStyle.Percent20, Color.Black, Color.GreenYellow);
+		hatchBrush = new HatchBrush(HatchStyle.Percent20, Color.Black, Color.DarkOliveGreen);
 		g.FillPath(hatchBrush, graphicPath);
 		//We are adding the dots to the image
 		for (int i = 0; i < (int)(rect.Width * rect.Height / 50F); i++)
@@ -105,4 +134,5 @@ public class Catchup : ICatchup
 		return bitmap;
 	}
 
+	#endregion
 }
